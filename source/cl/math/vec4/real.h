230a89381270d62a2a0557b010017203dbe42b53
deltasampler <deltasampler@gmail.com>
2025-05-03 08:06:32 +0300

    add vector color and packing functions

--- source/cl/math/vec4/real.h
+++ source/cl/math/vec4/real.h
@@ -1,3 +1,7 @@
+// color
+vec4_t pre(rgba)(num_t r = num_t(0.0), num_t g = num_t(0.0), num_t b = num_t(0.0), num_t a = num_t(1.0));
+vec4_t pre(hexa)(s32 h = 0);
+
 // basic
 void vec4_abs(vec4_t& out, const vec4_t& v);
 vec4_t vec4n_abs(const vec4_t& out);
